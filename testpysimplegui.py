5fe10d2b7bc7ec01bcd446c4aaf32331c2e478ee
ronaldochaves <ronald.chaves15@gmail.com>
2020-06-24 06:24:40 -0300

    Melhoria em boticário

--- testpysimplegui.py
+++ testpysimplegui.py
@@ -1,13 +1,22 @@
+# Headers
 import cv2
 import numpy as np
 #ffpyplayer for playing audio
 from ffpyplayer.player import MediaPlayer
-video_path = "C:\\Users\\renoi\\OneDrive\\Documentos\\RPG\\CODE\\media\\dices_1.mp4"
-def PlayVideo(video_path):
-    video=cv2.VideoCapture(video_path)
-    player = MediaPlayer(video_path)
+import os
+import os.path as osp
+
+# Path definitions
+currDir = osp.dirname(os.path.realpath(__file__))
+mediaDir = osp.join(currDir, 'media')
+videoPath = osp.join(mediaDir, 'dices_1.mp4')
+# video_path = "C:\\Users\\renoi\\OneDrive\\Documentos\\RPG\\CODE\\media\\dices_1.mp4"
+
+def PlayVideo(videoPath):
+    video = cv2.VideoCapture(videoPath)
+    player = MediaPlayer(videoPath)
     while True:
-        grabbed, frame=video.read()
+        grabbed, frame = video.read()
         audio_frame, val = player.get_frame()
         if not grabbed:
             print("End of video")
@@ -16,8 +25,8 @@ def PlayVideo(video_path):
             break
         cv2.imshow("Video", frame)
         if val != 'eof' and audio_frame is not None:
-            #audio
-            img, t = audio_frame
+            img, t = audio_frame                            # Audio
     video.release()
     cv2.destroyAllWindows()
-PlayVideo(video_path)
+
+PlayVideo(videoPath)
